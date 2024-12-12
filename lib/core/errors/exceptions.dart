f09948b273c646c948520961828de8384c247c67
Osama Yeslam <os7softgithub@gmail.com>
2024-12-12 19:27:11 +0300

    feat: initial clean_architecture structure

---
+++ lib/core/errors/exceptions.dart
@@ -0,0 +1,7 @@
+class ServerException implements Exception {}
+
+class LoginException implements Exception {}
+
+class NetworkException implements Exception {}
+
+class EmptyCachedException implements Exception {}
